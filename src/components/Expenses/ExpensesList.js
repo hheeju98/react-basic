81d760117e93d52887a46a1f31e2bf4e3af25623
hheeju98 <hheeju98@naver.com>
2023-01-10 01:09:25 +0900

    차트 완성

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -3,13 +3,11 @@ import ExpenseItem from "./ExpenseItem";
 import "./ExpensesList.css";
 
 const ExpensesList = (props) => {
-  let expensesContent = <p>No expenses found.</p>;
-
   if (props.items.length === 0) {
     return <h2 className="expenses-list__fallback">Found no expenses.</h2>;
   }
   return (
-    <ul className="expense-List">
+    <ul className="expenses-List">
       {props.items.map((expense) => (
         <ExpenseItem
           key={expense.id}
